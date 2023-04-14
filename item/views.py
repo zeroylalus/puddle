74b90315ec70b8381ae1a85bdf9fd2d21a1d8607
zeroylalus <armopc8@gmail.com>
2023-04-14 10:06:43 -0600

    o

--- item/views.py
+++ item/views.py
@@ -1,7 +1,28 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 from . import models
 from . import forms
+
+@login_required
+def items(request):
+    query = request.GET.get('query','')
+    category_id = request.GET.get('category',0)
+    categories = models.Category.objects.all()
+    items = models.Item.objects.filter(is_sold=False)
+    if category_id:
+        items = items.filter(category_id=category_id)
+    if query:
+        items = items.filter(
+            Q(name__icontains=query) |
+            Q(description__icontains=query)
+        )
+    return render(request, 'item/items.html',{
+        'items': items,
+        'query': query,
+        'categories': categories,
+        'category_id': int(category_id)
+    })
 
 def detail(request,pk):
     item = get_object_or_404(models.Item,pk=pk)
